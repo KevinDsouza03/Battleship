a87f01d97343106967026564648c85233f0078d1
vanessali824 <108141117+vanessali824@users.noreply.github.com>
2023-04-25 21:06:57 -0400

    Created scene after computer player type selected

--- Battleship/src/application/Main.java
+++ Battleship/src/application/Main.java
@@ -27,9 +27,10 @@ public class Main extends Application {
 			primaryStage.setTitle("BATTLESHIP");
 			
 			//add image
-			Image image1 = new Image("file:Battleship.jpg");
-			ImageView battleship = new ImageView(image1);
-			ImageView battleship2 = new ImageView(image1);
+			Image image = new Image("file:Battleship.jpg");
+			ImageView battleship = new ImageView(image);
+			ImageView battleship2 = new ImageView(image);
+			ImageView battleship3 = new ImageView(image);
 			
 			//add label to select player 2 or computer players
 			Label selection = new Label("Please select whether you would like to play against another player or the computer: ");
@@ -63,8 +64,10 @@ public class Main extends Application {
 			Label player1Name = new Label("Please enter Player 1's name: ");
 			player1Name.setStyle("-fx-text-fill: white");
 			
+			//textfield for player1 name
 			TextField name1 = new TextField();
 			
+			//Hbox for label and textfield
 			HBox firstName = new HBox(10, player1Name, name1);
 			
 			firstName.setPadding(new Insets(10));
@@ -73,26 +76,33 @@ public class Main extends Application {
 			Label player2Name = new Label("Please enter Player 2's name: ");
 			player2Name.setStyle("-fx-text-fill: white");
 			
+			//add textfield for player2's name
 			TextField name2 = new TextField();
 			
+			//hbox for label and textfield for player 2 name
 			HBox secondName = new HBox(10, player2Name, name2);
 			secondName.setPadding(new Insets(0, 0, 0, 10));
 			
+			//button to click to start game
 			Button mybutton = new Button("Start!");
 			mybutton.setStyle("-fx-background-color: teal; -fx-text-fill: white");
 
+			//Creating VBox for if 2 players are selected
 			VBox vbox = new VBox(10, battleship2, firstName, secondName, mybutton);
 			vbox.setAlignment(Pos.CENTER);
 			vbox.setStyle("-fx-background-color: black; -fx-font-weight: bold");
 			vbox.setPadding(new Insets(0, 0, 10, 0));
-			Scene scene = new Scene(vbox);
+			Scene scene2 = new Scene(vbox);
 			
 			selected.setOnAction(e -> 
 			{
+			//if choose to play against another player
 			if(player2.isSelected()) {
-				primaryStage.setScene(scene);
+				//change scene to ask for both players' names
+				primaryStage.setScene(scene2);
 				mybutton.setOnAction(event -> 
 				{
+					//storing textfield inputs
 					String nameFirstP, nameSecondP;
 					nameFirstP = name1.getText();
 					nameSecondP = name2.getText();
@@ -100,6 +110,26 @@ public class Main extends Application {
 					
 				}
 				);
+			}
+			else {
+				//if choose to play against computer
+				VBox vbox2 = new VBox(10, battleship3, firstName, mybutton);
+				
+				//styling vbox2
+				vbox2.setStyle("-fx-background-color: black; -fx-font-weight: bold");
+				vbox2.setAlignment(Pos.CENTER);
+				vbox2.setPadding(new Insets(0, 0, 10, 0));
+				
+				//creating new scene object
+				Scene scene3 = new Scene(vbox2);
+				
+				//setting scene
+				primaryStage.setScene(scene3);
+				
+				//storing textfield inputs
+				String nameFirst;
+				nameFirst = name1.getText();
+				System.out.println(nameFirst);
 			}
 			
 			}
