bb45cc1c0d630fcb02cf757faa8cf577564d5599
vanessali824 <108141117+vanessali824@users.noreply.github.com>
2023-04-25 20:44:22 -0400

    Adding scene to choose player type

--- Battleship/src/application/Main.java
+++ Battleship/src/application/Main.java
@@ -4,8 +4,10 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -25,8 +27,37 @@ public class Main extends Application {
 			primaryStage.setTitle("BATTLESHIP");
 			
 			//add image
-			Image image = new Image("file:Battleship.jpg");
-			ImageView battleship = new ImageView(image);
+			Image image1 = new Image("file:Battleship.jpg");
+			ImageView battleship = new ImageView(image1);
+			ImageView battleship2 = new ImageView(image1);
+			
+			//add label to select player 2 or computer players
+			Label selection = new Label("Please select whether you would like to play against another player or the computer: ");
+			selection.setStyle("-fx-text-fill: white");
+			
+			//add radio buttons
+			RadioButton player2 = new RadioButton("Human Player");
+			RadioButton computerPlayer = new RadioButton("Computer Player");
+			player2.setStyle("-fx-text-fill: white");
+			computerPlayer.setStyle("-fx-text-fill: white");
+			
+			//create toggle group
+			ToggleGroup radioGroup = new ToggleGroup();
+			player2.setToggleGroup(radioGroup);
+			computerPlayer.setToggleGroup(radioGroup);
+			
+			player2.setSelected(true);
+			
+			Button selected = new Button("Confirm");
+			selected.setStyle("-fx-background-color: teal; -fx-text-fill: white");
+			
+			VBox layout1 = new VBox(10, battleship, selection, player2, computerPlayer, selected);
+			layout1.setStyle("-fx-background-color: black; -fx-font-weight: bold");
+			layout1.setAlignment(Pos.CENTER);
+			layout1.setPadding(new Insets(0, 0, 10, 0));
+			
+			Scene scene1 = new Scene(layout1);
+			primaryStage.setScene(scene1);
 			
 			//add label for player1 name
 			Label player1Name = new Label("Please enter Player 1's name: ");
@@ -48,13 +79,32 @@ public class Main extends Application {
 			secondName.setPadding(new Insets(0, 0, 0, 10));
 			
 			Button mybutton = new Button("Start!");
+			mybutton.setStyle("-fx-background-color: teal; -fx-text-fill: white");
 
-			VBox vbox = new VBox(10, battleship, firstName, secondName, mybutton);
+			VBox vbox = new VBox(10, battleship2, firstName, secondName, mybutton);
 			vbox.setAlignment(Pos.CENTER);
 			vbox.setStyle("-fx-background-color: black; -fx-font-weight: bold");
 			vbox.setPadding(new Insets(0, 0, 10, 0));
 			Scene scene = new Scene(vbox);
-			primaryStage.setScene(scene);
+			
+			selected.setOnAction(e -> 
+			{
+			if(player2.isSelected()) {
+				primaryStage.setScene(scene);
+				mybutton.setOnAction(event -> 
+				{
+					String nameFirstP, nameSecondP;
+					nameFirstP = name1.getText();
+					nameSecondP = name2.getText();
+					System.out.println(nameFirstP + nameSecondP);
+					
+				}
+				);
+			}
+			
+			}
+			);
+			
 			
 			primaryStage.show();
 		} catch(Exception e) {
