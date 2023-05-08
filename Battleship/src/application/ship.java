4f0013e8c3e2de67a84a04822aba88ef7c348c46
vanessali824 <108141117+vanessali824@users.noreply.github.com>
2023-05-07 20:36:45 -0400

    Changed location to tile array

--- Battleship/src/application/ship.java
+++ Battleship/src/application/ship.java
@@ -1,45 +1,32 @@
 package application;
 
-import javafx.util.Pair;
 
 public class ship {
 	//size and location of ship
 	private int length;
-	private Pair<Integer, Integer> location[];
+	private tile location[];
 	//private tile shipLoc[];
-	private boolean sunk;
 	
 	/**
 	 * constructor that sets the size and location of the ship
 	 * @param len
 	 * @param loc
 	 */
-	public ship(int len, Pair<Integer, Integer> loc[]) {
+	public ship(int len, tile loc[]) {
 		length = len;
 		location = loc;
-		sunk = false;
 	}
 	
 	/**
 	 * checks if ship is sunk or not
 	 * @return whether ship sunk
 	 */
-	public boolean isSunk(gameBoard gb) {
-		if (sunk) {return true;}
-		for (Pair<Integer,Integer>i : location) {
-			if (gb.getTileisOccupied(i.getKey(), i.getValue() ) && gb.getTileisHit(i.getKey(), i.getValue())) {
-				//if both of these are true, go to the next iteration
-				continue;
-			}
-			else { // if at any point, either of those are false, return false
-				return false;
-			}
+	public boolean isSunk() {
+		for(int i = 0; i < length; i++){
+			if(!location[i].isHit()) return false;
 		}
-		// if we exit the for loop, we are sunk so update sunk for only a one time iteration, and return true.
-		sunk = true;
 		return true;
 	}
-
 
 	/**
 	 * getter function that returns location
@@ -50,10 +37,10 @@ public class ship {
 	}
 
 	/**
-	 * getter function that returns locations of ship. 
+	 * getter function that returns location
 	 * @return
 	 */
-	public Pair<Integer, Integer>[] getLocation() {
+	public tile[] getLocation() {
 		return location;
 	}
 
@@ -61,7 +48,7 @@ public class ship {
 	 * sets location of ship
 	 * @param location
 	 */
-	public void setLocation(Pair<Integer, Integer> location[]) {
+	public void setLocation(tile location[]) {
 		this.location = location;
 	}
 
