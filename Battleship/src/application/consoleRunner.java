3c9c2d7a1c22b8feb756e212eadd25531048784b
KevinDsouza03 <dsouzakev467@gmail.com>
2023-05-10 18:00:10 -0400

    added humanvscomputer

--- Battleship/src/application/consoleRunner.java
+++ Battleship/src/application/consoleRunner.java
@@ -38,9 +38,18 @@ public class consoleRunner {
 		//now play game
 		gameBoard p1Board = new gameBoard(player1);
 		gameBoard p2Board = new gameBoard(compPlayer1);
-		play(player1,p1Board,compPlayer1,p2Board);
+//		play(player1,p1Board,compPlayer1,p2Board);
+//		
+//		
 		
+	}
+	
+	public consoleRunner(humanPlayer human, computerPlayer computer, gameBoard hBoard, gameBoard cBoard,int x, int y) {
 		
+		//now play game
+		humanvsComputer(human,hBoard,computer,cBoard,x,y);
+//		
+//		
 		
 	}
 	/***
@@ -53,7 +62,8 @@ public class consoleRunner {
 	public void play(player p1, gameBoard p1Board, player p2, gameBoard p2Board) {
 		Random rand = new Random();
 //		Scanner scanner = new Scanner(System.in);
-		while(!(p1Board.checkWin(p1) || p2Board.checkWin(p2))) {
+		///while(!(p1Board.checkWin(p1) || p2Board.checkWin(p2))) {
+		for (int i = 0; i < 50; i++) {
 			System.out.println("Player1 Board");
 			p1Board.printBoard();
 			System.out.println("Player2 Board");
@@ -72,6 +82,25 @@ public class consoleRunner {
 		System.out.println("Player2 Final Board");
 		p2Board.printBoard();
 		System.out.println("Game Over");
+	}
+	
+	public void humanvsComputer(humanPlayer p1, gameBoard p1Board, computerPlayer p2, gameBoard p2Board,int x, int y) {
+		Random rand = new Random();
+		for (int i = 0; i < 50; i++) {
+			System.out.println("Player1 Board");
+			p1Board.printBoard();
+			System.out.println("Player2 Board");
+			p2Board.printBoard();
+			System.out.println("Player1 First Move. Prompting user to play");
+			p1.fire(x, y, p2Board);
+			System.out.println("Player 1 fired");
+			System.out.println("Player2 play");
+			p2.fire(rand.nextInt(10), rand.nextInt(10), p1Board);
+		}
+		System.out.println("Player1 After Fire");
+		p1Board.printBoard();
+		System.out.println("Player2 After Fire");
+		p2Board.printBoard();
 	}
 
 	
