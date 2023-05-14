ef869181db017ae9cb142d4e6528f5d61bcef496
vanessali824 <108141117+vanessali824@users.noreply.github.com>
2023-05-14 10:42:27 -0400

    fixing merge issue

--- Battleship/src/application/bomb.java
+++ Battleship/src/application/bomb.java
@@ -10,7 +10,6 @@ public class bomb implements specialMove{
 	public void specialAttack(int x, int y, gameBoard attack) {
 		// TODO Auto-generated method stub
 		attack.getTile(x, y).updateHit(true);
-<<<<<<< HEAD
 		attack.getTile(x, y).setFill(Color.AQUA);
 		if((x+1) <= 10) {
 			attack.getTile(x+1, y).updateHit(true);
@@ -19,28 +18,11 @@ public class bomb implements specialMove{
 			if((y+1) <= 10) {
 				attack.getTile(x+1, y+1).updateHit(true);
 				attack.getTile(x+1, y+1).setFill(Color.AQUA);
-=======
-		if(attack.hitAShip(x, y)) {
-			attack.getTile(x, y).setFill(Color.ORANGE);
-		}
-		else {
-			attack.getTile(x, y).setFill(Color.KHAKI);
-		}
-		if((x+1) <= 10) {
-			attack.getTile(x+1, y).updateHit(true);
-		
-			
-			if((y+1) <= 10) {
-				attack.getTile(x+1, y+1).updateHit(true);
->>>>>>> acc7035f765cfaf57dff4344d5b4a6f4f5aa049b
 				
 			}
 			if((y-1) >= 0) {
 				attack.getTile(x+1, y-1).updateHit(true);
-<<<<<<< HEAD
 				attack.getTile(x+1, y-1).setFill(Color.AQUA);
-=======
->>>>>>> acc7035f765cfaf57dff4344d5b4a6f4f5aa049b
 			
 			}
 			
@@ -48,54 +30,32 @@ public class bomb implements specialMove{
 		
 		if((x-1) >= 0) {
 			attack.getTile(x-1, y).updateHit(true);
-<<<<<<< HEAD
 			attack.getTile(x-1, y).setFill(Color.AQUA);
 			
 			if((y+1) <= 10) {
 				attack.getTile(x-1, y+1).updateHit(true);
 				attack.getTile(x-1, y+1).setFill(Color.AQUA);
-=======
-			
-			
-			if((y+1) <= 10) {
-				attack.getTile(x-1, y+1).updateHit(true);
->>>>>>> acc7035f765cfaf57dff4344d5b4a6f4f5aa049b
 				
 			}
 			if((y-1) >= 0) {
 				attack.getTile(x-1, y-1).updateHit(true);
-<<<<<<< HEAD
 				attack.getTile(x-1, y-1).setFill(Color.AQUA);
-=======
->>>>>>> acc7035f765cfaf57dff4344d5b4a6f4f5aa049b
 				
 			}
 		}
 		
 		if((y+1) <= 10) {
 			attack.getTile(x, y+1).updateHit(true);
-<<<<<<< HEAD
 			attack.getTile(x, y+1).setFill(Color.AQUA);
-=======
-			
->>>>>>> acc7035f765cfaf57dff4344d5b4a6f4f5aa049b
 			
 		}
 			
 		
 		if((y-1) >= 0) {
 			attack.getTile(x, y-1).updateHit(true);
-<<<<<<< HEAD
 			attack.getTile(x, y-1).setFill(Color.AQUA);
-=======
-			
->>>>>>> acc7035f765cfaf57dff4344d5b4a6f4f5aa049b
 			
 		}
 		}
 }
-<<<<<<< HEAD
 	
-=======
-	
->>>>>>> acc7035f765cfaf57dff4344d5b4a6f4f5aa049b
